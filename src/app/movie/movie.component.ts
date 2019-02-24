9422e7e01eb903dc7e05cb7b8eedf7c045066386
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-02-24 12:53:33 +0100

    utworzenie komponentu movie

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wjs-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent {}
