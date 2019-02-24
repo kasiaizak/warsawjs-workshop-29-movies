cf3c73fe7e96a100a1925e057a37602904e6d4b8
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-02-24 13:01:54 +0100

    dodanie 1 filmu

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from './movie';
 
 @Component({
   selector: 'wjs-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent {}
+
+export class MovieComponent {
+  movie: Movie = {
+    id: 1,
+    title: 'Subiektywny przegląd sytuacji na froncie - meet.js Poznań',
+    description: '"Nowy dzień, nowy framework". Jak żyć?',
+    url: 'https://www.youtube.com/embed/Aybh_2pIi2I'
+  }
+}
