bc01b95a75c3d56869855779c21ba64d845b0ced
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-02-24 14:40:29 +0100

    sformatowanie pod material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
@@ -10,7 +13,10 @@ import { MovieComponent } from './movie/movie.component';
     MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
