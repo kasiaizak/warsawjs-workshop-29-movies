2956c8fe433bb2265155a1bd8a9a093418a7ac68
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-02-24 18:00:39 +0100

    dodanie filtra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -18,9 +20,11 @@ import { MoviesComponent } from './movies/movies.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     HttpClientModule,
+    MatButtonModule,
     MatCardModule,
-    MatButtonModule
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
